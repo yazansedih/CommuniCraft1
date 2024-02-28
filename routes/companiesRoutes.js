e9ecd989c7a0a8b890c71e7b8610b9dc7dbefc7d
yazansedih <s12010059@stu.najah.edu>
2024-02-28 18:21:43 +0200

    add companies Features.

---
+++ routes/companiesRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const companiesController = require('../controllers/companiesController');
+const { authenticateUser } = require('../middlewares/authenticateUser');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+// Routes for user registration and authentication
+// router.post('/', companiesController.yourFunction); //api/companies/
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
