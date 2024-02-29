9f4dbad3b6d8f84ede5eaefa1ec1fa0083d96ec1
lujaindarwazeh <lujain.darwazeh123@gmail.com>
2024-02-29 21:22:22 +0200

    lujain

--- routes/resourcesRoutes.js
+++ routes/resourcesRoutes.js
@@ -5,8 +5,14 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-// Routes for user registration and authentication
-// router.post('/', resourcesController.yourFunction); //api/resources/
+router.post('/addResource',authenticateUser, resourcesController.addResource);
+router.put('/updateResource/:id', authenticateUser,resourcesController.updateResource);
+router.delete('/deleteResource/:id',authenticateUser, resourcesController.deleteResource);
+
+
+router.post('/printAll', authenticateUser,resourcesController.printAll);
+router.get('/availableResource',authenticateUser,resourcesController.availableResource);
+router.get('/ResourceMaxcoast/:maxcoast',authenticateUser,resourcesController.ResourceMaxcoast);
 
 
 
