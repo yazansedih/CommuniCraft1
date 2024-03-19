46fc1ed3da9840132bcb0a0ddef9c5bee6c0b052
Laith Shakhshir <107434672+LaithShakhshir@users.noreply.github.com>
2024-03-19 21:25:03 +0200

    Update finishedprojectRepository.js

--- data/database/finishedprojectRepository.js
+++ data/database/finishedprojectRepository.js
@@ -1,5 +1,5 @@
+
 const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -9,8 +9,34 @@ const db = mysql.createConnection({
 });
 
 class FinishedprojectRepository {
-    // write your functions ...
+  // Función para obtener el informe del sistema
+  getSystemReport(callback) {
+    const sqlQuery = `
+      SELECT 
+        COUNT(UserID) AS TotalUsers,
+        SUM(CASE WHEN Active = 1 THEN 1 ELSE 0 END) AS ActiveUsers,
+        SUM(CASE WHEN Active = 0 THEN 1 ELSE 0 END) AS InactiveUsers,
+        COUNT(DISTINCT GroupID) AS TotalGroups
+      FROM 
+        users;
+    `;
 
+    db.query(sqlQuery, (err, results) => {
+      if (err) {
+        console.error('Error fetching system report:', err);
+        return callback(err, null);
+      }
+
+      const systemReport = {
+        totalUsers: results[0].TotalUsers,
+        activeUsers: results[0].ActiveUsers,
+        inactiveUsers: results[0].InactiveUsers,
+        totalGroups: results[0].TotalGroups
+      };
+
+      callback(null, systemReport);
+    });
+  }
 }
 
 module.exports = FinishedprojectRepository;
