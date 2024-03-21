08a91af6bc760219bbcfc8d59e4aaa73abed7967
yazansedih <s12010059@stu.najah.edu>
2024-03-22 01:07:29 +0200

    add external api.

--- data/database/externalAPIsRepository.js
+++ data/database/externalAPIsRepository.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const axios = require('axios');
+const http = require('node:http');
 require('dotenv').config();
 
 
@@ -12,6 +13,7 @@ const db = mysql.createConnection({
 });
 
 class ExternalAPIsRepository {
+
     weather(req, res) {
         const { workshopid } = req.params; 
         
@@ -42,9 +44,83 @@ class ExternalAPIsRepository {
         });
     }
 
-
-
+    news(req, res) {
+        const { workshopid } = req.params; 
+        
+        const sql = 'SELECT * FROM localpartnerships WHERE WorkshopID = ?';
+        db.query(sql, [workshopid], async (error, results) => {
+            if (error) {
+                console.error('Error searching for groups:', error);
+                return res.status(500).json({ error: 'Internal server error' });
+            }
     
+            const data = results[0].WorkshopName;
+            
+            const apiKey = "158006db2acd4881ab5b3264e1737177";
+            const APIUrl = `https://newsapi.org/v2/everything?q=${data}&from=2024-02-21&sortBy=publishedAt&apiKey=${apiKey}`;
+            let news;
+    
+            try {
+                const response = await axios.get(APIUrl);
+                news = response.data;
+            } catch(err) {
+                news = null;
+                console.error('Error fetching news data:', err);
+                return res.status(500).json({ error: 'Failed to fetch news data!😢' });
+            }
+    
+            res.status(200).json({ news });
+        });
+    }
+
+    userData(req, res) {
+        // Initialize ipAddress variable
+        let ipAddress = '';
+    
+        // Create options for the HTTP request
+        const options = {
+            host: "api.ipify.org",
+            path: "/"
+        };
+    
+        http.get(options, (response) => {
+            response.setEncoding('utf8');
+    
+            response.on('data', (chunk) => {
+                ipAddress += chunk;
+            });
+            
+            response.on('end', () => {
+                const { userId } = req.session;
+
+                const sql = 'SELECT * FROM users WHERE UserID = ?';
+                db.query(sql, [userId], async (error, results) => {
+                    if (error) {
+                        console.error('Error searching for users:', error);
+                        return res.status(500).json({ error: 'Internal server error' });
+                    }
+                    
+                    const apiKey = "at_ZExKAjnJyDEBTVmdSe3NH7C0Wp3eI";
+                    const APIUrl = `https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+                    let data;
+    
+                    try {
+                        const response = await axios.get(APIUrl);
+                        data = response.data;
+                    } catch(err) {
+                        data = null;
+                        console.error('Error fetching data:', err);
+                        return res.status(500).json({ error: 'Failed to fetch data!😢' });
+                    }
+    
+                    res.status(200).json({ data });
+                });
+            });
+        }).on('error', (error) => {
+            console.error(`Error retrieving IP address: ${error.message}`);
+            return res.status(500).json({ error: 'Failed to retrieve IP address!😢' });
+        });
+    }
 
 }
 
