04388c25a55f377a012b168048aea7a318dd2850
yazansedih <s12010059@stu.najah.edu>
2024-03-23 22:55:45 +0200

    update finished...

--- controllers/finishedprojectController.js
+++ controllers/finishedprojectController.js
@@ -3,35 +3,13 @@
 const FinishedprojectRepository = require('../data/database/finishedprojectRepository');
 const finishedprojectRepository = new FinishedprojectRepository();
 
-// Controlador para obtener el informe del sistema
-exports.getSystemReport = (req, res) => {
-  finishedprojectRepository.getSystemReport((err, systemReport) => {
-    if (err) {
-      console.error('Error fetching system report:', err);
-      return res.status(500).json({ error: 'Error fetching system report' });
-    }
-    res.json(systemReport);
-  });
-};
 
-exports.getAllFinishedProjects=(req,res)=>{
-  finishedprojectRepository.allFinishedProjects((err, systemReport) => {
-    if (err) {
-      console.error('Error fetching system report:', err);
-      return res.status(500).json({ error: 'Error fetching system report' });
-    }
-    res.json(systemReport);
-  });
+exports.AllFinishedProjects=(req,res)=>{
+  finishedprojectRepository.allFinishedProjects(req, res);
 };
 
 exports.getFnishedProjectById=(req,res)=>{
-  finishedprojectRepository.allFinishedProjects((err, systemReport) => {
-    if (err) {
-      console.error('Error fetching system report:', err);
-      return res.status(500).json({ error: 'Error fetching system report' });
-    }
-    res.json(systemReport);
-  });
+  finishedprojectRepository.allFinishedProjects(req, res);
 };
 
 
--- data/database/finishedprojectRepository.js
+++ data/database/finishedprojectRepository.js
@@ -1,59 +1,30 @@
-
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'advdatabase',
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "advdatabase",
 });
 
 class FinishedprojectRepository {
-  // Función para obtener el informe del sistema
-  getSystemReport(callback) {
-    const sqlQuery = `
-      SELECT 
-        COUNT(UserID) AS TotalUsers,
-        SUM(CASE WHEN Active = 1 THEN 1 ELSE 0 END) AS ActiveUsers,
-        SUM(CASE WHEN Active = 0 THEN 1 ELSE 0 END) AS InactiveUsers,
-        COUNT(DISTINCT GroupID) AS TotalGroups
-      FROM 
-        users;
-    `;
 
-    db.query(sqlQuery, (err, results) => {
-      if (err) {
-        console.error('Error fetching system report:', err);
-        return callback(err, null);
-      }
-
-      const systemReport = {
-        totalUsers: results[0].TotalUsers,
-        activeUsers: results[0].ActiveUsers,
-        inactiveUsers: results[0].InactiveUsers,
-        totalGroups: results[0].TotalGroups
-      };
-
-      callback(null, systemReport);
-    });
-  }
   allFinishedProjects(req, res) {
-
-    const sql = "SELECT * FROM craftprojects Status = 1";
+    const sql = "SELECT * FROM craftprojects WHERE Status = 1";
     db.query(sql, (error, results) => {
-      if (error) {
-        console.error("Error finished project:", error);
-        return res.status(500).json({ error: "Internal server error" });
-      }
+        if (error) {
+            console.error("Error fetching finished projects:", error);
+            return res.status(500).json({ error: "Internal server error" });
+        }
 
-      if (results.length === 0) {
-        return res.status(404).json({ message: "There is no finished projects!😢" });
-      }
-
-      return res.status(200).json({ finishedProjects: results });
+        if (results.length === 0) { 
+            return res.status(404).json({ message: "There are no finished projects" });
+        }
+        
+        return res.status(200).json({ finishedProjects: results });
     });
   }
-
 
   finishedProjectById(req, res) {
 
